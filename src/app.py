81e1431456d315a64ddc07f26acafeeee963956a
ssimkani <seenasimkani@gmail.com>
2025-04-26 13:21:54 -0400

    Load required files for app

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import json
 
 # Streamlit UI
 def main():
@@ -13,3 +14,16 @@ def main():
         layout="centered",
         initial_sidebar_state="collapsed",
     )
+
+    # Load the trained model
+    model = joblib.load("../models/rf_model.pkl")
+
+    # load the encoded df
+    with open("../data/processed/encodings.json", "r") as enc_file:
+        df_encodings = json.load(enc_file)
+
+    # Load Styles
+    with open("../style/style.css", "r") as css_file:
+        css = css_file.read()
+
+    st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
