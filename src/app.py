e5d24e7da06f2d9b64fe8d524b1b4bb35fda2c83
ssimkani <seenasimkani@gmail.com>
2025-04-26 13:53:42 -0400

    Add sliders and dropdowns for user input

--- src/app.py
+++ src/app.py
@@ -9,21 +9,82 @@ def main():
 
     # Set page config
     st.set_page_config(
-        page_title="Heart Disease Risk Calculator",
+        page_title="Heart Disease Risk Predictor",
         page_icon="❤️",
         layout="centered",
         initial_sidebar_state="collapsed",
     )
 
     # Load the trained model
-    model = joblib.load("../models/rf_model.pkl")
+    model = joblib.load("models/rf_model.pkl")
 
     # load the encoded df
-    with open("../data/processed/encodings.json", "r") as enc_file:
+    with open("data/processed/encodings.json", "r") as enc_file:
         df_encodings = json.load(enc_file)
 
     # Load Styles
-    with open("../style/style.css", "r") as css_file:
+    with open("style/style.css", "r") as css_file:
         css = css_file.read()
 
     st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+
+    st.title("Heart Disease Risk Predictor")
+    st.write("Predict the risk of heart disease based on various health metrics.")
+
+    # Age
+    age = st.slider("Age", 0, 120, 30)
+
+    # Sex
+    sex = st.selectbox("Sex", ["Male", "Female"], placeholder="Select Sex", index=None)
+
+    # Chest Pain Type
+    chest_pain_type = st.selectbox(
+        "Chest Pain Type",
+        ["Typical Angina", "Atypical Angina", "Non-Anginal Pain", "Asymptomatic"],
+        placeholder="Select Chest Pain Type",
+        index=None,
+    )
+
+    # Resting Blood Pressure
+    resting_blood_pressure = st.slider("Resting Blood Pressure (mmHG)", 0, 200, 120)
+
+    # Serum Cholesterol
+    serum_cholesterol = st.slider("Serum Cholesterol (mg/dl)", 0, 600, 200)
+
+    # Maximum Heart Rate
+    max_heart_rate = st.slider("Maximum Heart Rate", 0, 250, 150)
+
+    # Exercise Induced Angina
+    exercise_induced_angina = st.selectbox(
+        "Exercise Induced Angina",
+        ["Yes", "No"],
+        placeholder="Exercise Induced Angina?",
+        index=None,
+    )
+
+    # Oldpeak
+    oldpeak = st.slider("Oldpeak", 0.0, 10.0, 2.0)
+
+    # ST Slope
+    st_slope = st.selectbox(
+        "ST Slope",
+        ["Up", "Flat", "Down"],
+        placeholder="Select ST Slope",
+        index=None,
+    )
+
+    # Make sure all fields are filled
+    all_fields_filled = (
+    sex is not None and
+    chest_pain_type is not None and
+    exercise_induced_angina is not None and
+    st_slope is not None
+    )
+
+    if all_fields_filled:
+        if st.button("Predict Heart Disease Risk"):
+            # Make prediction
+            pass
+
+if __name__ == "__main__":
+    main()
